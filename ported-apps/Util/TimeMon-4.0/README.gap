e7f556b1abd5f5d92380210e4667e14b769759b9
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2004-02-02 03:40:13 +0000

    Adding the baseline of TimeMon to the repository.   This app will be ported to GNUstep in days to come.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@30 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ ported-apps/Util/TimeMon-4.0/README.gap
@@ -0,0 +1,7 @@
+This program is not under the GPL.   However it is released with a license 
+that is compatible with free software.
+
+I have always found TimeMon to be useful and I want to port it to Linux using
+GNUstep.
+
+Thanks, GJC
---
+++ ported-apps/Util/TimeMon-4.0/TimeMonWraps.m
@@ -0,0 +1,68 @@
+// translated from the pswraps for GNUstep/MOSX by Gregory John Casamento
+
+#include <AppKit/PSOperators.h>
+
+// define the colors...
+#define IDLE   PSsetrgbcolor(1.0,1.0,1.0)
+#define NICE   PSsetrgbcolor(0.667,0.667,0.667)
+#define USER   PSsetrgbcolor(0.333,0.333,0.333)
+#define SYSTEM PSsetrgbcolor(0.0,0.0,0.0)
+
+// the drawing functions...
+void _doDrawArc1(double radius, double bdeg, double ddeg)
+{
+  // white circle...
+  IDLE;
+  PSmoveto(24,24);
+  PSarc(24,24,radius,0,360);
+  PSfill();
+
+  // Dark gray "pie" slice.
+  USER;
+  PSmoveto(24,24);
+  PSarcn(24,24,radius,90,4);
+  PSfill();
+
+  // Black slice.
+  SYSTEM;
+  PSmoveto(24,24);
+  PSarcn(24,24,radius,90,4);
+  PSfill();
+}
+
+void _doDrawArc2(double radius, double bdeg, double ddeg, double ldeg)
+{
+  // white circle...
+  IDLE;
+  PSmoveto(24,24);
+  PSarc(24,24,radius,0,360);
+  PSfill();
+
+  // Light gray "pie" slice.
+  NICE;
+  PSmoveto(24,24);
+  PSarcn(24,24,radius,90,4);
+  PSfill();
+
+  // Dark gray "pie" slice.
+  USER;
+  PSmoveto(24,24);
+  PSarcn(24,24,radius,90,4);
+  PSfill();
+
+  // Black slice.
+  SYSTEM;
+  PSmoveto(24,24);
+  PSarcn(24,24,radius,90,4);
+  PSfill();
+}
+
+void drawArc1(double radius, double bdeg, double ddeg)
+{
+  _doDrawArc1(radius,bdeg,ddeg);
+}
+
+void drawArc2(double radius, double bdeg, double ddeg, double ldeg)
+{
+  _doDrawArc2(radius,bdeg,ddeg,ldeg);
+}
