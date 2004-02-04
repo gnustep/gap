466166924865fc59bbb9bb2babff5aa434082532
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2004-02-04 01:58:37 +0000

    Updating GUI and adding data source.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@31 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ system-apps/NFSConfig/NFSClientDataSource.h
@@ -0,0 +1,8 @@
+/* All Rights reserved */
+
+#include <AppKit/AppKit.h>
+
+@interface NFSClientDataSource : NSObject
+{
+}
+@end
---
+++ system-apps/NFSConfig/NFSClientDataSource.m
@@ -0,0 +1,8 @@
+/* All Rights reserved */
+
+#include <AppKit/AppKit.h>
+#include "NFSClientDataSource.h"
+
+@implementation NFSClientDataSource
+
+@end
