9aba85ef5bc112a26bf1e8672abd04e44672d4c8
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2005-01-29 18:37:55 +0000

    Made conformant to style guidelines.
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@76 d25b4930-3292-e311-b074-0010c6de5da8

--- ported-apps/Util/TimeMon/NSColorExtensions.m
+++ ported-apps/Util/TimeMon/NSColorExtensions.m
@@ -8,22 +8,31 @@
 {
     float r, g, b, a;
     NSArray *array = [colorString componentsSeparatedByString:@" "];
-    if(!array) return nil;
-    if([array count] < 3) {
+
+    if (!array) 
+      {
+	return nil;
+      }
+
+    if ([array count] < 3) 
+      {
         NSLog(@"%@: + colorFromStringRepresentation", [[self class] description]);
         NSLog(@"%@: String must contain red, green, and blue components", [[self class] description]);
         return nil;
-    }
+      }
+
     r = [[array objectAtIndex:0] floatValue];
     g = [[array objectAtIndex:1] floatValue];
     b = [[array objectAtIndex:2] floatValue];
     a = [array count] > 3 ? [[array objectAtIndex:3] floatValue] : 1.0;
+
     return [NSColor colorWithCalibratedRed:r green:g blue:b alpha:a];
 }
 
 - (NSString *)stringRepresentation
 {
     float r, g, b, a;
+
     [[self colorUsingColorSpaceName:NSCalibratedRGBColorSpace] getRed:&r green:&g blue:&b alpha:&a];
     return [NSString stringWithFormat:@"%f %f %f %f",r,g,b,a];
 }
