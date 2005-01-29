c529e937bddf4c135f72527ac330d78b850f546d
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2005-01-29 21:59:30 +0000

    More improvements.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@77 d25b4930-3292-e311-b074-0010c6de5da8

--- ported-apps/Util/TimeMon/TimeMonWraps.h
+++ ported-apps/Util/TimeMon/TimeMonWraps.h
@@ -2,19 +2,5 @@
 
 #include <AppKit/PSOperators.h>
 
-// misc PS functions.
-void drawInit();
-
-void PSWait();
-
-void setColor(char *n, float r, float g, float b);
-
 // the drawing functions...
-void _doDrawArc1(double radius, double bdeg, double ddeg);
-
-void _doDrawArc2(double radius, double bdeg, double ddeg, double ldeg);
-
-void drawArc1(double radius, double bdeg, double ddeg);
-
 void drawArc2(double radius, double bdeg, double ddeg, double ldeg);
-
