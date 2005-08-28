7870fb98128cf1b85642246867dc5bdaad274c58
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2005-08-28 02:16:39 +0000

    Added "standard_install=no" to innerspace module makefiles.   And added code for PPC front-end application.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@221 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/PPC/PPCController.m
@@ -0,0 +1,56 @@
+/* All Rights reserved */
+
+#include <AppKit/AppKit.h>
+#include "PPCController.h"
+
+@implementation PPCController
+
+
+- (void) setSlavePath: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) ok: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) revert: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) setMasterPath: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) showOutput: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) setPPCUserHome: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) setPathToPPC: (id)sender
+{
+  /* insert your code here */
+}
+
+
+- (void) startEmulator: (id)sender
+{
+  /* insert your code here */
+}
+
+@end
