438567e568fdc9b71c7033ee22cd259b4df945eb
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2006-03-22 23:29:33 +0000

    initial import
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@267 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/LaternaMagica/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LaternaMagica
+//
+//  Created by Riccardo Mottola on Sun Jan 15 2006.
+//  Copyright (c) 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/NSApplication.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
