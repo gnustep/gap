9c09e31ffc1512cea5602adde097e3bdb696c621
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2006-06-11 01:03:44 +0000

    new custom image view
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@283 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/LaternaMagica/LMFlipView.h
@@ -0,0 +1,16 @@
+//
+//  LMFlipView.h
+//  LaternaMagica
+//
+//  Created by Riccardo Mottola on Sun Jun 11 2006.
+//  Copyright (c) 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+@interface LMFlipView : NSImageView {
+
+}
+
+@end
