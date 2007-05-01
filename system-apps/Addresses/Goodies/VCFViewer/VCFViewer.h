eb9d0cff315083da3d7fecfe8c68256be8386ac7
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2007-05-01 23:08:38 +0000

    added missing stuff
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@343 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ system-apps/Addresses/Goodies/VCFViewer/VCFViewer.h
@@ -0,0 +1,45 @@
+// VCFViewer.h (this is -*- ObjC -*-)
+// 
+// \author: Björn Giesler <giesler@ira.uka.de>
+// 
+// VCF Content Viewer for GWorkspace
+// 
+// $Author: rmottola $
+// $Locker:  $
+// $Revision: 1.1 $
+// $Date: 2007/05/01 23:07:09 $
+
+#ifndef VCFVIEWER_H
+#define VCFVIEWER_H
+
+/* system includes */
+#include <Foundation/Foundation.h>
+#include <GWorkspace/ContentViewersProtocol.h>
+#include <Addresses/Addresses.h>
+#include <AddressView/ADPersonView.h>
+
+/* my includes */
+/* (none) */
+
+@interface VCFViewer: NSView <ContentViewersProtocol>
+{
+  id panel;
+  NSArray *people; int currentPerson;
+
+  NSScrollView *sv; NSClipView *cv; ADPersonView *pv;
+  NSButton *nb, *pb; NSTextField *lbl;
+  NSButton *ifb, *dfb;
+
+  NSString *bundlePath;
+
+  int index;
+}
+
+- (void) nextPerson: (id) sender;
+- (void) previousPerson: (id) sender;
+
+- (void) increaseFontSize: (id) sender;
+- (void) decreaseFontSize: (id) sender;
+@end
+
+#endif /* VCFVIEWER_H */
