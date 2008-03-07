7d1abfc17ffeb277c3174340bc968cb6085675f8
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-03-07 23:46:59 +0000

    use editor and not object
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@462 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Graphos/GRDocView.m
+++ user-apps/Graphos/GRDocView.m
@@ -515,7 +515,7 @@ float zFactors[9] = {0.25, 0.5, 1, 1.5, 2, 3, 4, 6, 8};
             }
         } else
         {
-            [obj unselect];
+            [[obj editor] unselect];
         }
     }
     [self setNeedsDisplay: YES];
