6b135c32d28d6164f57a97ef7e3a81d82bacc168
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-03-11 00:36:31 +0000

    Added copyright headers
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@474 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Graphos/GRObjectControlPoint.m
+++ user-apps/Graphos/GRObjectControlPoint.m
@@ -1,10 +1,27 @@
-//
-//  GRObjectControlPoint.m
-//  Graphos
-//
-//  Created by Riccardo Mottola on Tue Sep 18 2007.
-//  Copyright (c) 2007 __MyCompanyName__. All rights reserved.
-//
+/*
+ Project: Graphos
+ GRObjectControlPoint.m
+
+ Copyright (C) 2007-2008 GNUstep Application Project
+
+ Author: Ing. Riccardo Mottola
+
+ Created: 2007-11-18
+
+ This application is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+
+ This application is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+
+ You should have received a copy of the GNU General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #import "GRObjectControlPoint.h"
 
