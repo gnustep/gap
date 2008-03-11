bca9585b00460d0823a29a1f8c1d103cc9fa38cc
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-03-11 23:58:19 +0000

    added bounds calulcating function
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@489 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Graphos/GRFunctions.h
+++ user-apps/Graphos/GRFunctions.h
@@ -4,6 +4,6 @@
 NSPoint pointApplyingCostrainerToPoint(NSPoint p, NSPoint sp);
 
 BOOL pointInRect(NSRect rect, NSPoint p);
-
+NSRect GRMakeBounds(float x, float y, float width, float height);
 double grmin(double a, double b);
 double grmax(double a, double b);
