2302d6dde5d9149c86505dde47ca838f76c91d41
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-03-14 21:51:50 +0000

    header with license and cleanup
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@495 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Graphos/GRBezierControlPoint.m
+++ user-apps/Graphos/GRBezierControlPoint.m
@@ -1,3 +1,28 @@
+/*
+ Project: Graphos
+ GRBezierControlPoint.h
+
+ Copyright (C) 2000-2008 GNUstep Application Project
+
+ Author: Enrico Sersale (original GDraw implementation)
+ Author: Ing. Riccardo Mottola
+
+ This application is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+
+ This application is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+
+ You should have received a copy of the GNU General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
 #import "GRBezierControlPoint.h"
 #import "GRBezierPathEditor.h"
 #import "GRFunctions.h"
@@ -92,7 +117,8 @@
             bzHandle.secondHandle.y = bzHandle.center.y + disty;
     }
 
-    if(pointInRect(bzHandle.secondHandleRect, oldp)) {
+    if(pointInRect(bzHandle.secondHandleRect, oldp))
+    {
         bzHandle.secondHandle = newp;
         distx = grmax(bzHandle.secondHandle.x, bzHandle.center.x) - grmin(bzHandle.secondHandle.x, bzHandle.center.x);
         disty = grmax(bzHandle.secondHandle.y, bzHandle.center.y) - grmin(bzHandle.secondHandle.y, bzHandle.center.y);
