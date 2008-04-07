bb658077e1f268e001bac6feb26f69a4ca5a2007
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-04-07 19:01:37 +0000

    editor superclass for NSPathObjects
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@513 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/Graphos/GRPathEditor.m
@@ -0,0 +1,40 @@
+/*
+ Project: Graphos
+ GRPathEditor.m
+
+ Copyright (C) 2008 GNUstep Application Project
+
+ Author: Ing. Riccardo Mottola
+
+ Created: 2008-03-30
+
+ This application is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+
+ This application is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+
+ You should have received a copy of the GNU General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#import "GRPathEditor.h"
+
+
+@implementation GRPathEditor
+
+- (NSPoint)moveControlAtPoint:(NSPoint)p
+{
+}
+
+- (void)moveControlAtPoint:(NSPoint)oldp toPoint:(NSPoint)newp
+{
+}
+
+
+@end
