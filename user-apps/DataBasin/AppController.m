5320f06ccbce4627eef8c4c8c36a9c19270d6437
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2008-11-18 00:22:01 +0000

    initial commit DataBasin
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@609 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/DataBasin/AppController.m
@@ -0,0 +1,100 @@
+/* 
+   Project: DataBasin
+
+   Copyright (C) 2008 Free Software Foundation
+
+   Author: Riccardo Mottola,,,
+
+   Created: 2008-11-13 22:44:02 +0100 by multix
+   
+   Application Controller
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+ 
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#import "AppController.h"
+#import "DBSoap.h"
+
+@implementation AppController
+
++ (void)initialize
+{
+  NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
+
+  /*
+   * Register your app's defaults here by adding objects to the
+   * dictionary, eg
+   *
+   * [defaults setObject:anObject forKey:keyForThatObject];
+   *
+   */
+  
+  [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (id)init
+{
+  if ((self = [super init]))
+    {
+    }
+  return self;
+}
+
+- (void)dealloc
+{
+  [super dealloc];
+}
+
+- (void)awakeFromNib
+{
+  [[NSApp mainMenu] setTitle:@"DataBasin"];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotif
+{
+}
+
+- (BOOL)applicationShouldTerminate:(id)sender
+{
+  return YES;
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotif
+{
+}
+
+- (BOOL)application:(NSApplication *)application openFile:(NSString *)fileName
+{
+}
+
+- (void)showPrefPanel:(id)sender
+{
+}
+
+- (IBAction)doLogin:(id)sender
+{
+  DBSoap   *db;
+  NSString *userName;
+  NSString *password;
+  
+  userName = [fieldUserName stringValue];
+  password = [fieldPassword stringValue];
+  
+  db =[[DBSoap alloc] init];
+  [db login :userName :password];
+}
+
+@end
