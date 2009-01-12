f940be4b5e7ff98fcadd106d49e3a0fec2b5ef29
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2009-01-12 00:15:35 +0000

    prelminimary static bookmark menu support
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@665 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Vespucci/VEMenuItem.h
+++ user-apps/Vespucci/VEMenuItem.h
@@ -1,42 +1,42 @@
 /*
  Project: Vespucci
- VEAppController.h
-
- Copyright (C) 2007-2009
-
+ VEMenuItem.h
+ 
+ Copyright (C) 2009
+ 
  Author: Ing. Riccardo Mottola
-
- Created: 2007-06-26
-
+ 
+ Created: 2009-01-11
+ 
  This application is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public
  License as published by the Free Software Foundation; either
  version 2 of the License, or (at your option) any later version.
-
+ 
  This application is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Library General Public License for more details.
-
+ 
  You should have received a copy of the GNU General Public
  License along with this library; if not, write to the Free
  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#import <Foundation/Foundation.h>
+
 #import <AppKit/AppKit.h>
 
 
-@interface VEAppContoller : NSObject
+@interface VEMenuItem : NSMenuItem
 {
-    IBOutlet NSPanel *prefPanel;
-    IBOutlet NSTextField *homePageField;
-    IBOutlet NSMenuItem *bookmarksMenu;
+    NSString *url;
+    NSString *title;
 }
 
-- (IBAction) showPreferences:(id)sender;
-- (IBAction) savePrefs:(id)sender;
-- (IBAction) cancelPrefs:(id)sender;
-- (IBAction) loadBookmark:(id)sender;
+- (NSString *)url;
+- (NSString *)title;
+- (void)setUrl:(NSString *)urlString;
+- (void)setUrlTitle:(NSString *)titleString;
+
 
 @end
--- user-apps/Vespucci/VEMenuItem.m
+++ user-apps/Vespucci/VEMenuItem.m
@@ -1,42 +1,62 @@
 /*
  Project: Vespucci
- VEAppController.h
-
- Copyright (C) 2007-2009
-
+ VEMenuItem.h
+ 
+ Copyright (C) 2009
+ 
  Author: Ing. Riccardo Mottola
-
- Created: 2007-06-26
-
+ 
+ Created: 2009-01-11
+ 
  This application is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public
  License as published by the Free Software Foundation; either
  version 2 of the License, or (at your option) any later version.
-
+ 
  This application is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Library General Public License for more details.
-
+ 
  You should have received a copy of the GNU General Public
  License along with this library; if not, write to the Free
  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#import <Foundation/Foundation.h>
-#import <AppKit/AppKit.h>
+
+#import "VEMenuItem.h"
 
 
-@interface VEAppContoller : NSObject
+@implementation VEMenuItem
+
+- (NSString *)url
 {
-    IBOutlet NSPanel *prefPanel;
-    IBOutlet NSTextField *homePageField;
-    IBOutlet NSMenuItem *bookmarksMenu;
+    return url;
 }
 
-- (IBAction) showPreferences:(id)sender;
-- (IBAction) savePrefs:(id)sender;
-- (IBAction) cancelPrefs:(id)sender;
-- (IBAction) loadBookmark:(id)sender;
+- (NSString *)title
+{
+    return title;
+}
+
+- (void)setUrl:(NSString *)urlString
+{
+    [url release];
+    url = [urlString retain];
+}
+
+- (void)setUrlTitle:(NSString *)titleString
+{
+    [title release];
+    title = [titleString retain];
+    [super setTitle:title];
+}
+
+- (void)dealloc
+{
+    [url release];
+    [title release];
+    [super dealloc];
+}
 
 @end
