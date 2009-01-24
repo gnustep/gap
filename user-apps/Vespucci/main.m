fc4d78d67065e930aa6967fff0a37b37500df97e
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2009-01-24 14:43:57 +0000

    fixed document types for html
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@684 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Vespucci/main.m
+++ user-apps/Vespucci/main.m
@@ -1,10 +1,25 @@
-//
-//  main.m
-//  myAfrica
-//
-//  Created by Dr. H. Nikolaus Schaller on Wed Jul 30 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
-//
+/*
+ Project: Vespucci
+ VEWinController.m
+
+ Copyright (C) 2007-2008
+
+ Author: Ing. Riccardo Mottola, Dr. H. Nikolaus Schaller
+
+ This application is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+
+ This application is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+
+ You should have received a copy of the GNU General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #import <Cocoa/Cocoa.h>
 
