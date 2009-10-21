2d2668e36abbd686f19813d00de2d90e5eaf0f7a
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2009-10-21 17:14:33 +0000

    use correct path functions
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@949 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Grr/Components/TreeDatabase/TreeDatabaseComponent.m
+++ user-apps/Grr/Components/TreeDatabase/TreeDatabaseComponent.m
@@ -1,7 +1,8 @@
 /*
    Grr RSS Reader
    
-   Copyright (C) 2006, 2007 Guenther Noack <guenther@unix-ag.uni-kl.de>
+   Copyright (C) 2006 - 2007 Guenther Noack <guenther@unix-ag.uni-kl.de>
+   2009 Riccardo Mottola, GNUstep Application Project
    
    This application is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
@@ -182,11 +183,15 @@
     static NSString* dbPath = nil;
     
     if (dbPath == nil) {
-        NSString* path = [@"~/GNUstep/Library/Grr" stringByExpandingTildeInPath];
+        NSString* path;
         
         NSFileManager* manager = [NSFileManager defaultManager];
         
         BOOL isDir, exists;
+
+        path = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject];
+        path = [path stringByAppendingPathComponent:[[NSProcessInfo processInfo] processName]];
+
         exists = [manager fileExistsAtPath: path isDirectory: &isDir];
         
         if (exists) {
@@ -199,7 +204,7 @@
             }
         }
         
-        ASSIGN(dbPath, [path stringByAppendingString: @"/database.plist"]);
+        ASSIGN(dbPath, [path stringByAppendingPathComponent: @"database.plist"]);
     }
     
     return dbPath;
