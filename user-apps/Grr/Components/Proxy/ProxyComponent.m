e2fc29d553569c4cb579177771d59a2a8e2032a0
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2009-11-01 09:51:12 +0000

    changed and uniformed license to GPLv3+ with consent of authors
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@966 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Grr/Components/Proxy/ProxyComponent.m
+++ user-apps/Grr/Components/Proxy/ProxyComponent.m
@@ -2,20 +2,24 @@
    Grr RSS Reader
    
    Copyright (C) 2006, 2007 Guenther Noack <guenther@unix-ag.uni-kl.de>
-   
+   Copyright (C) 2009  GNUstep Application Team
+                       Riccardo Mottola
+
    This application is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
-   License version 2 as published by the Free Software Foundation.
-   
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+ 
    This application is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
-   
+ 
    You should have received a copy of the GNU General Public
    License along with this library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA. 
 */
+
 
 #import "ProxyComponent.h"
 
--- user-apps/Grr/ImageTextCell.h
+++ user-apps/Grr/ImageTextCell.h
@@ -1,14 +1,24 @@
 /*
-**  ImageTextCell.h
-**
-**  Copyright (c) 2003-2004
-**
-**  Author: Ludovic Marcotte <ludovic@Sophos.ca>
-**
-**
-**  This application is free software; you can redistribute it and/or 
-**  modify it under the terms of the MIT license. See COPYING.
+   ImageTextCell.h
+   
+   Copyright (c) 2003-2004
+   Author: Ludovic Marcotte <ludovic@Sophos.ca>
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+ 
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA. 
 */
+
 
 #ifndef _GNUMail_H_ImageTextCell
 #define _GNUMail_H_ImageTextCell
