b068e399ae2006a73de92897fd51e6d3bb5c7638
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2011-01-21 23:19:47 +0000

    Track delete, backspace, up and down events
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1392 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/LaternaMagica/LMFlipView.m
+++ user-apps/LaternaMagica/LMFlipView.m
@@ -2,7 +2,7 @@
    Project: LaternaMagica
    LMFlipView.m
 
-   Copyright (C) 2006-2010 Riccardo Mottola
+   Copyright (C) 2006-2011 Riccardo Mottola
 
    Author: Riccardo Mottola
 
@@ -28,12 +28,20 @@
 
 #ifdef GNUSTEP
 #define KC_ESCAPE 9
-#define KC_LEFTARROW 100
-#define KC_RIGHTARROW 102
+#define KC_LEFTARROW 113
+#define KC_RIGHTARROW 114
+#define KC_UPARROW 111
+#define KC_DOWNARROW 116
+#define KC_DELETE 119
+#define KC_BACKSPACE 22
 #else
 #define KC_ESCAPE 53
 #define KC_LEFTARROW 123
 #define KC_RIGHTARROW 124
+#define KC_UPARROW 126
+#define KC_DOWNARROW 125
+#define KC_DELETE 117
+#define KC_BACKSPACE 51
 #endif
 
 @implementation LMFlipView
@@ -53,7 +61,9 @@
 /** respond to key equivalents which are not bound do menu items */
 -(BOOL)performKeyEquivalent: (NSEvent*)theEvent
 {
-    unsigned short keyCode;
+  unsigned short keyCode;
+  unsigned int modifierFlags;
+
 #ifdef __APPLE__
 /* Apple is definitively broken here and on all versions tested it returns for the arrow key
     also a KeyUp event, which it should not, as the Event is specified to be keyDown */
@@ -62,23 +72,33 @@
 #endif
 
     keyCode = [theEvent keyCode];
+    modifierFlags = [theEvent modifierFlags];
     if (keyCode == KC_ESCAPE)
       {
         NSLog(@"(keyCode) Escape!");
         [controller setFullScreen:theEvent];
         return YES;
       }
-    else if (keyCode == KC_LEFTARROW)
+    else if (keyCode == KC_LEFTARROW || keyCode == KC_UPARROW)
       {
         [controller prevImage:theEvent];
         return YES;
       }
-    else if (keyCode == KC_RIGHTARROW)
+    else if (keyCode == KC_RIGHTARROW || keyCode == KC_DOWNARROW)
       {
         [controller nextImage:theEvent];
         return YES;
       }
-    NSLog(@"keyCode %d", keyCode);    
+    else if (keyCode == KC_BACKSPACE || keyCode == KC_DELETE)
+      {
+	if (modifierFlags & NSShiftKeyMask)
+	  [controller removeImage:theEvent];
+	else
+	  [controller eraseImage:theEvent];
+        return YES;
+      }
+    else
+      NSLog(@"keyCode %d", keyCode);    
     return [super performKeyEquivalent:theEvent];
 }
 
