b81c9dd72c8e486dd283f842dd96c9a1f12ecf35
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2011-08-22 23:42:41 +0000

    Oresme Example Cartesius
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1562 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ libs/Oresme/Examples/Cartesius/AppController.m
@@ -0,0 +1,75 @@
+/* 
+   Project: Cartesius
+
+   Author: Riccardo Mottola
+
+   Created: 2011-08-23 01:18:46 +0200 by multix
+   
+   Application Controller
+*/
+
+#import <OresmeKit/OKCartesius.h>
+
+#import "AppController.h"
+
+@implementation AppController
+
++ (void) initialize
+{
+  NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
+
+  /*
+   * Register your app's defaults here by adding objects to the
+   * dictionary, eg
+   *
+   * [defaults setObject:anObject forKey:keyForThatObject];
+   *
+   */
+  
+  [[NSUserDefaults standardUserDefaults] registerDefaults: defaults];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (id) init
+{
+  if ((self = [super init]))
+    {
+    }
+  return self;
+}
+
+- (void) dealloc
+{
+  [super dealloc];
+}
+
+- (void) awakeFromNib
+{
+}
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif
+{
+// Uncomment if your application is Renaissance-based
+//  [NSBundle loadGSMarkupNamed: @"Main" owner: self];
+}
+
+- (BOOL) applicationShouldTerminate: (id)sender
+{
+  return YES;
+}
+
+- (void) applicationWillTerminate: (NSNotification *)aNotif
+{
+}
+
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName
+{
+  return NO;
+}
+
+- (void) showPrefPanel: (id)sender
+{
+}
+
+@end
