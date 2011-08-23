bcb9de7a1636db02ddb0e7548333dd1beee0d636
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2011-08-23 21:38:52 +0000

    oresme kit
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1564 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ libs/Oresme/OresmeKit/OKCartesius.m
@@ -0,0 +1,65 @@
+/*
+   Project: OresmeKit
+   
+   Carthesius: Abscissa/Ordinata Charts
+
+   Copyright (C) 2011 Free Software Foundation
+
+   Author: Riccardo Mottola
+
+   Created: 2011-08-21 23:58:53 +0200 by multix
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#import <AppKit/AppKit.h>
+
+#import "OKCartesius.h"
+
+@implementation OKCartesius
+
+-(id)initWithFrame: (NSRect)frameRect
+{
+  self = [super initWithFrame: frameRect];
+  if (self)
+    {
+      quadrantPositioning = OKQuadrantCentered;
+      NSLog(@"Inited");
+    }
+  return self;
+}
+
+-(void)drawRect: (NSRect)rect
+{
+  NSPoint origo;
+  NSRect boundsRect;
+  
+  NSLog(@"Draw");
+  origo = NSMakePoint(0, 0);
+  boundsRect = [self bounds];
+  if (quadrantPositioning == OKQuadrantCentered)
+    {
+      origo = NSMakePoint(boundsRect.size.width/2, boundsRect.size.height/2);
+    }
+    
+  [[NSColor whiteColor] set];
+  [NSBezierPath fillRect: [self bounds]];
+  
+  [[NSColor blackColor] set];
+  [NSBezierPath strokeRect: NSMakeRect(0, origo.y, boundsRect.size.width, origo.y)];
+  [NSBezierPath strokeRect: NSMakeRect(origo.x, 0, origo.x, boundsRect.size.height)];
+}
+
+@end
