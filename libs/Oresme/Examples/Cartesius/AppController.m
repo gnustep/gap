a2e65181c4e31237c76f2b5e1da8c64e155f047a
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2011-09-21 12:26:48 +0000

    simplified
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1596 d25b4930-3292-e311-b074-0010c6de5da8

--- libs/Oresme/Examples/Cartesius/AppController.m
+++ libs/Oresme/Examples/Cartesius/AppController.m
@@ -14,21 +14,6 @@
 
 @implementation AppController
 
-+ (void) initialize
-{
-  NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
-
-  /*
-   * Register your app's defaults here by adding objects to the
-   * dictionary, eg
-   *
-   * [defaults setObject:anObject forKey:keyForThatObject];
-   *
-   */
-  
-  [[NSUserDefaults standardUserDefaults] registerDefaults: defaults];
-  [[NSUserDefaults standardUserDefaults] synchronize];
-}
 
 - (id) init
 {
@@ -166,20 +151,6 @@
 //  [NSBundle loadGSMarkupNamed: @"Main" owner: self];
 }
 
-- (BOOL) applicationShouldTerminate: (id)sender
-{
-  return YES;
-}
-
-- (void) applicationWillTerminate: (NSNotification *)aNotif
-{
-}
-
-- (BOOL) application: (NSApplication *)application
-	    openFile: (NSString *)fileName
-{
-  return NO;
-}
 
 - (void) showPrefPanel: (id)sender
 {
