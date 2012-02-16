7acf67a373c2347814ef4f17f5dd2930fdb01e22
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-02-16 15:06:21 +0000

    Migrated preferences GUI to Gorm, fixed typos.
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1714 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/PreferencesController.h
+++ system-apps/Zipper/PreferencesController.h
@@ -4,23 +4,27 @@
 
 @interface PreferencesController : NSObject
 {
-    IBOutlet NSPanel *_panel;
-	IBOutlet NSButton *_bsdTarButon;
-	IBOutlet NSTextField *_defaultOpenApp;
+  IBOutlet NSPanel *_panel;
+  IBOutlet NSButton *_bsdTarButton;
+  IBOutlet NSTextField *_defaultOpenApp;
 
-	IBOutlet NSTextField *_tarTextField;
-	IBOutlet NSTextField *_unzipTextField;
-	IBOutlet NSTextField *_rarTextField;
-	IBOutlet NSTextField *_lhaTextField;
-	IBOutlet NSTextField *_lzxTextField;
-	IBOutlet NSTextField *_sevenZipTextField;
+  IBOutlet NSTextField *_tarTextField;
+  IBOutlet NSTextField *_unzipTextField;
+  IBOutlet NSTextField *_rarTextField;
+  IBOutlet NSTextField *_lhaTextField;
+  IBOutlet NSTextField *_lzxTextField;
+  IBOutlet NSTextField *_sevenZipTextField;
 	
-	// this holds a reference to an Archive subclass that the user
-	// needs to set before he can leave the prefs dialog
-	id _archiveClass;
+  // this holds a reference to an Archive subclass that the user
+  // needs to set before he can leave the prefs dialog
+  id _archiveClass;
 }
 
 - (void)showPreferencesPanel;
-		
+- (IBAction)cancelPressed:(id)sender;
+- (IBAction)okPressed:(id)sender;
+- (IBAction)findExecutable:(id)sender;
+- (IBAction)findDefaultOpenApp:(id)sender;
+
 @end
 
