58845fb1bf0104dd2bc1f2783a26d8640bb8dded
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-02-20 09:27:17 +0000

    Do not open an untitled document. Add boilerplate
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1732 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/AppDelegate.h
+++ system-apps/Zipper/AppDelegate.h
@@ -1,15 +1,32 @@
+/*
+
+  AppDelegate.h
+  Zipper
+
+  Copyright (C) 2012 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/NSObject.h>
 
 @class NSTableView, Archive, TableViewDataSource;
 
 @interface AppDelegate : NSObject
 {
-  @private
-//	IBOutlet NSTableView *_tableView;
 
-//	NSString *_filename;
-//	Archive *_archive;
-//	TableViewDataSource *_tableViewDataSource;
 }
 
 - (void)showPreferences:(id)sender;
