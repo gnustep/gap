7657806cface23d5de84a1b79b19cc928f9e716d
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-02-20 16:38:25 +0000

    Add boilerplate
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1734 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/FileInfo.h
+++ system-apps/Zipper/FileInfo.h
@@ -1,3 +1,24 @@
+/*
+
+  FileInfo.h
+  Zipper
+
+  Copyright (C) 2012 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/NSObject.h>
 
 @class NSString, NSCalendarDate, NSNumber;
@@ -5,11 +26,11 @@
 @interface FileInfo : NSObject
 {
   @private
-	NSString *_path;
-	NSString *_filename;
+    NSString *_path;
+    NSString *_filename;
     NSCalendarDate  *_date;
     NSNumber *_size;
-	NSString *_ratio;
+    NSString *_ratio;
 }
 
 + (FileInfo *)newWithPath:(NSString *)path date:(NSCalendarDate *)date size:(NSNumber *)size;
