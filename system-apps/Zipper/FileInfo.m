dae9071f8119851e28230fff7d87e67bdef70e8f
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-02-20 16:38:25 +0000

    Add boilerplate
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1734 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/FileInfo.m
+++ system-apps/Zipper/FileInfo.m
@@ -1,3 +1,24 @@
+/*
+
+  FileInfo.m
+  Zipper
+
+  Copyright (C) 2012 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/Foundation.h>
 #import "FileInfo.h"
 #import "NSString+Custom.h"
