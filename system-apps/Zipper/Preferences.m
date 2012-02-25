be283d99e83672889abfe6935150a5fd5c4b7d5b
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-02-25 13:42:09 +0000

    Remove ASSIGN macro, add boiler plate
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1754 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/Preferences.m
+++ system-apps/Zipper/Preferences.m
@@ -1,3 +1,25 @@
+/*
+
+  Preferences.m
+  Zipper
+
+  Copyright (C) 2012 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/Foundation.h>
 #import "Preferences.h"
 #import "NSFileManager+Custom.h"
@@ -60,7 +82,9 @@ static NSMutableDictionary *_extensionMapping = nil;
 
 + (void)usePreferences:(NSDictionary *)newPrefs;
 {
-	ASSIGN(_replacementPrefs, newPrefs);
+  [_replacementPrefs release];
+  _replacementPrefs = newPrefs;
+  [_replacementPrefs retain];
 }
 
 + (NSString *)tarExecutable;
