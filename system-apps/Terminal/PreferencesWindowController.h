f766f4999b5bc7f183d1eb79c9d806c60253e2c4
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-03-26 09:44:24 +0000

    header import cleanup
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1783 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Terminal/PreferencesWindowController.h
+++ system-apps/Terminal/PreferencesWindowController.h
@@ -1,5 +1,9 @@
 /*
-copyright 2002 Alexander Malmberg <alexander@malmberg.org>
+Copyright 2002 Alexander Malmberg <alexander@malmberg.org>
+          2012 Free Software Foundation, Inc
+
+Authros: Alexander Malmberg
+         Riccardo Mottola
 
 This file is a part of Terminal.app. Terminal.app is free software; you
 can redistribute it and/or modify it under the terms of the GNU General
@@ -7,12 +11,9 @@ Public License as published by the Free Software Foundation; version 2
 of the License. See COPYING or main.m for more information.
 */
 
-#ifndef PreferencesWindowController_h
-#define PreferencesWindowController_h
+#import <AppKit/NSWindowController.h>
 
-#include <AppKit/NSWindowController.h>
-
-#include "PrefBox.h"
+#import "PrefBox.h"
 
 @class GSHbox,NSBox;
 
@@ -31,5 +32,4 @@ of the License. See COPYING or main.m for more information.
 
 @end
 
-#endif
 
