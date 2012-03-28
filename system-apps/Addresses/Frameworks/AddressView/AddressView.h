5fd78ea04976e18055450e50b97dcf74f3f9f7ad
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-03-28 12:50:56 +0000

    Change include to import
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1798 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Addresses/Frameworks/AddressView/AddressView.h
+++ system-apps/Addresses/Frameworks/AddressView/AddressView.h
@@ -1,24 +1,11 @@
 // AddressView.h (this is -*- ObjC -*-)
 // 
-// \author: Björn Giesler <giesler@ira.uka.de>
+// Author: Björn Giesler <giesler@ira.uka.de>
 // 
 // Address View Framework for GNUstep
-// 
-// $Author: rmottola $
-// $Locker:  $
-// $Revision: 1.1 $
-// $Date: 2007/03/29 22:36:04 $
 
-#ifndef ADDRESSVIEW_H
-#define ADDRESSVIEW_H
+#import <Addresses/Addresses.h>
 
-/* system includes */
-#include <Addresses/Addresses.h>
 
-/* my includes */
-/* (none) */
-
-#include <AddressView/ADPersonView.h>
-#include <AddressView/ADSinglePropertyView.h>
-
-#endif /* ADDRESSVIEW_H */
+#import <AddressView/ADPersonView.h>
+#import <AddressView/ADSinglePropertyView.h>
