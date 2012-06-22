7bf254005332682f0e71e296bef0f65bfc1fdf1d
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-06-22 10:17:00 +0000

    add logger class
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1939 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/DataBasin/DBLogger.m
@@ -0,0 +1,62 @@
+/*
+   Project: DataBasin
+
+   Copyright (C) 2012 Free Software Foundation
+
+   Author: Riccardo Mottola
+
+   Created: 2012-04-24 10:50:19 +0000 by multix
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#import "DBLogger.h"
+
+@implementation DBLogger
+
+- (id)init
+{
+  if ((self = [super init]))
+    {
+      [NSBundle loadNibNamed:@"Log" owner:self];
+    }
+  return self;
+}
+
+-(void)dealloc
+{
+  [super dealloc];
+}
+
+
+- (IBAction)show:(id)sender
+{
+  [logWin makeKeyAndOrderFront:self];
+}
+
+-(void)log: (DBLogLevel)level :(NSString* )format, ...
+{
+  va_list ap;
+  NSString *formattedString;
+
+  va_start (ap, format);
+  formattedString = [[NSString alloc] initWithFormat:format arguments: ap];
+  va_end(ap);
+  
+  NSLog(@"level: %d | %@", level, formattedString);
+  [formattedString release];
+}
+
+@end
