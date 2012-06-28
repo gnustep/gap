fbb782b2420f38583542ee7847e21fbebbc3b7ea
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2012-06-28 15:48:04 +0000

    Initial commit of Berkelium framework skeleton to GAP.
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@1943 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ libs/Berkelium/Berkelium.h
@@ -0,0 +1,38 @@
+/*
+   Project: Berkelium
+
+   Copyright (C) 2012 Free Software Foundation
+
+   Author: Gregory John Casamento,,,
+
+   Created: 2012-06-24 18:56:36 -0400 by heron
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#ifndef _BERKELIUM_H_
+#define _BERKELIUM_H_
+
+#import <Foundation/Foundation.h>
+
+@interface Berkelium : NSObject
+{
+
+}
+
+@end
+
+#endif // _BERKELIUM_H_
+
