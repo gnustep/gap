49ad66e7cf3b2456bae5e822e2bd47ba8eadc8ab
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2012-06-28 23:21:09 +0000

    Changes added.  Test application implemented.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@1946 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ libs/Berkelium/BerkeliumTest/BerkeliumTestController.h
@@ -0,0 +1,11 @@
+/* All Rights reserved */
+
+#include <Foundation/Foundation.h>
+#include <AppKit/AppKit.h>
+
+@interface BerkeliumTestController : NSObject
+{
+  IBOutlet NSWindow *window;
+}
+
+@end
---
+++ libs/Berkelium/BerkeliumTest/BerkeliumTestController.m
@@ -0,0 +1,9 @@
+/* All Rights reserved */
+
+#include <AppKit/AppKit.h>
+#include <Foundation/Foundation.h>
+
+#include "BerkeliumTestController.h"
+
+@implementation BerkeliumTestController
+@end
