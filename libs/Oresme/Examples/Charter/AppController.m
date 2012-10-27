35aae343297827d91737ac052675a7530eaafc21
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2012-10-27 15:57:06 +0000

    add 3rd series and change of plot type
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@2075 d25b4930-3292-e311-b074-0010c6de5da8

--- libs/Oresme/Examples/Charter/AppController.m
+++ libs/Oresme/Examples/Charter/AppController.m
@@ -46,6 +46,7 @@
 {
   OKSeries *series1;
   OKSeries *series2;
+  OKSeries *series3;
   int i;
   float v1, v2;
 
@@ -74,16 +75,62 @@
       [series2 release];
       for (i = 0; i < 6; i++)
 	{
-	  v1 = pow(i, 1.5);
-	  v2 = pow(i, 2);
+	  v1 = pow(i, 1.5)+0.1;
+	  v2 = pow(i, 2)+0.2;
 
 	  [series1 addObject: [NSNumber numberWithFloat: v1]];
 	  [series2 addObject: [NSNumber numberWithFloat: v2]];
 	}
-      NSLog(@"series 1 calculated");
+      NSLog(@"series 2 calculated");
+    }
+  else if ([[sender selectedItem] tag] == 2)
+    {
+      [series1 setColor: [NSColor purpleColor]];
+      series2 = [[OKSeries alloc] init];
+      [series2 setColor: [NSColor greenColor]];
+      [chartView addSeries: series2];
+      [series2 release];
+      series3 = [[OKSeries alloc] init];
+      [series3 setColor: [NSColor yellowColor]];
+      [chartView addSeries: series3];
+      [series3 release];
+
+      [series1 addObject: [NSNumber numberWithFloat: 20]];
+      [series2 addObject: [NSNumber numberWithFloat: 80]];
+      [series3 addObject: [NSNumber numberWithFloat: 100]];
     }
   NSLog(@"redisplay");
   [chartView setNeedsDisplay: YES];
+}
+
+- (IBAction)changeChartType:(id)sender
+{
+  NSRect rect;
+  OKChart *tempChart;
+  NSView *superView;
+
+  rect = [chartView frame];
+  superView = [chartView superview];
+
+  if ([[sender selectedItem] tag] == 0)
+    {
+      tempChart = [[OKLineChart alloc] initWithFrame: rect];
+    }
+  else if ([[sender selectedItem] tag] == 1)
+    {
+      tempChart = [[OKPieChart alloc] initWithFrame: rect];
+    }
+  else
+    {
+      NSLog(@"Unexpected chart type");
+      return;
+    }
+  
+  [tempChart setAutoresizingMask:[chartView autoresizingMask]];
+  [chartView removeFromSuperview];
+  chartView = tempChart;
+  [superView addSubview: chartView];
+  
 }
 
 - (IBAction) changeBackgroundColor: (id)sender
