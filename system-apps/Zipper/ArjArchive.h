0e12d99216a56966ca9718fd2bd0856db13e7fbd
buzzdee <buzzdee@d25b4930-3292-e311-b074-0010c6de5da8>
2012-12-31 21:49:37 +0000

    * ArjArchive.[h|m] add them, and make watching contents and           extraction work unsing unarj         * ZipperInfo.plist, Archive.m AppDelegate.m add ArjArchive hooks
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@2157 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ system-apps/Zipper/ArjArchive.h
@@ -0,0 +1,11 @@
+#import <Foundation/NSObject.h>
+#import "Archive.h"
+
+@interface ArjArchive : Archive
+{
+}
+
+- (int)expandFiles:(NSArray *)files withPathInfo:(BOOL)usePathInfo toPath:(NSString *)path;
+- (NSArray *)listContents;
+
+@end
