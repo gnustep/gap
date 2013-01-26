9a2e57360ecb1ddfec748e853510e32ea1109cbe
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2013-01-26 17:49:55 +0000

    Return ArchiveType and not NSString
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@2195 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/TarArchive.m
+++ system-apps/Zipper/TarArchive.m
@@ -1,3 +1,46 @@
+/*
+
+  Archive.h
+  Zipper
+
+  Copyright (C) 2012 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ *//*
+
+  Archive.h
+  Zipper
+
+  Copyright (C) 2012-2013 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+           Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 #import "TarArchive.h"
@@ -38,9 +81,9 @@
 	return NO;
 }
 
-+ (NSString *)archiveType
++ (ArchiveType)archiveType
 {
-	return @"tar";
+	return TAR;
 }
 
 //------------------------------------------------------------------------------
