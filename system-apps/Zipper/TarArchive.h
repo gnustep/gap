99ae283587eae1b275e913c12de4336064045565
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2013-01-26 23:03:56 +0000

    Fix boiler plate
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@2197 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/TarArchive.h
+++ system-apps/Zipper/TarArchive.h
@@ -1,3 +1,26 @@
+/*
+
+  TarArchive.h
+  Zipper
+
+  Copyright (C) 2012-2013 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+           Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/NSObject.h>
 #import "Archive.h"
 
