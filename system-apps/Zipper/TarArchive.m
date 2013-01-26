5bb6c4bf4e73f9136d3ea492f8cc390115a1d012
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2013-01-26 23:03:56 +0000

    Fix boiler plate
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@2197 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/TarArchive.m
+++ system-apps/Zipper/TarArchive.m
@@ -1,26 +1,6 @@
 /*
 
-  Archive.h
-  Zipper
-
-  Copyright (C) 2012 Free Software Foundation, Inc
-
-  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
-           Riccardo Mottola <rm@gnu.org>
-
-  This application is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by the Free
-  Software Foundation; either version 2 of the License, or (at your option)
-  any later version.
-
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU General Public License for more details
-
- *//*
-
-  Archive.h
+  TarArchive.m
   Zipper
 
   Copyright (C) 2012-2013 Free Software Foundation, Inc
