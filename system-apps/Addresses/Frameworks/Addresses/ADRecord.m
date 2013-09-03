11f22293227535a24eebee9609c061d14f8f81b2
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2013-09-03 09:11:11 +0000

    Cast the result of copy which is itself to avoid warning.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@2527 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Addresses/Frameworks/Addresses/ADRecord.m
+++ system-apps/Addresses/Frameworks/Addresses/ADRecord.m
@@ -124,7 +124,7 @@
 
 - (id) copyWithZone: (NSZone*) z
 {
-  ADRecord* obj = NSCopyObject(self, 0, z);
+  ADRecord* obj = (ADRecord *)NSCopyObject(self, 0, z);
   obj->_readOnly = _readOnly;
 
   // delete UID if it has one
