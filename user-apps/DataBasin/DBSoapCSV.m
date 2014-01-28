ab78c26db4d8e5f2ae4b21f641485318cd22ff92
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2014-01-28 18:14:53 +0000

    updated license
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@2635 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/DataBasin/DBSoapCSV.m
+++ user-apps/DataBasin/DBSoapCSV.m
@@ -1,26 +1,27 @@
 /*
-   Project: DataBasin
+  Project: DataBasin
 
-   Copyright (C) 2008-2013 Free Software Foundation
+  Copyright (C) 2008-2013 Free Software Foundation
 
-   Author: Riccardo Mottola
+  Author: Riccardo Mottola
 
-   Created: 2011-11-08 22:44:45 +0100 by multix
+  Created: 2011-11-08 22:44:45 +0100 by multix
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
- 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
- 
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
- */
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02111 USA.
+*/
 
 #import <AppKit/AppKit.h>
 
