92b208ba4ee184e7d4d5c154e0a6d2429b331f92
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2014-11-13 19:11:31 +0000

    old mac define
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@2771 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/DataBasin/DBTextFormatter.h
+++ user-apps/DataBasin/DBTextFormatter.h
@@ -24,6 +24,10 @@
 
 #import <Foundation/Foundation.h>
 
+#if !defined (GNUSTEP) &&  (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4)
+#define NSUInteger unsigned
+#endif
+
 @interface DBTextFormatter : NSFormatter
 {
   NSUInteger maxLength;
