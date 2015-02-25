14f0590f77d3913354232863390cc928fc5fecd8
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2015-02-25 17:55:12 +0000

    new DCCTransfer superclass for DCCSender and DCCGetter, further cleanup
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@2949 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ user-apps/TalkSoup/InFilters/DCCSupport/DCCTransfer.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                                DCCTransfer.h
+                          -------------------
+    begin                : 25 Feb 2015
+    copyright            : (C) 2015 by Riccardo Mottola
+                         :             The GNUstep Application Team
+    email                : rm@gnu.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#import <Foundation/NSObject.h>
+#include <stdint.h>
+
+@class DCCObject;
+
+@interface DCCTransfer : NSObject
+{
+  NSFileHandle *file;
+  NSString *path;
+  NSString *status;
+  id connection;
+  id delegate;
+  NSTimer *cpsTimer;
+  int cps;
+  uint32_t oldTransferredBytes;
+}
+
+- (NSString *)status;
+
+- (id)localHost;
+- (id)remoteHost;
+
+- (int)cps;
+
+- (NSString *)path;
+
+
+@end
