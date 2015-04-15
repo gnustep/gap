0474ac0e362c8498f9745dcceabd92e81152846b
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2015-04-15 06:33:42 +0000

    better definitions
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3028 d25b4930-3292-e311-b074-0010c6de5da8

--- libs/DataBasinKit/DBCSVReader.h
+++ libs/DataBasinKit/DBCSVReader.h
@@ -27,7 +27,12 @@
 #import <Foundation/Foundation.h>
 
 #if defined(__APPLE__) && (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4)
+#ifndef NSUInteger
 #define NSUInteger unsigned int
+#endif
+#ifndef NSInteger
+#define NSInteger int
+#endif
 #endif
 
 @protocol DBLoggerProtocol;
