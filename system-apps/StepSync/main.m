3935bf904c192147371008bc504456088261b28f
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2017-02-02 23:38:04 +0000

    initial StepSync skeleton for Panther import
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3376 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ system-apps/StepSync/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StepSync
+//
+//  Created by Riccardo Mottola on 2/2/17.
+//  Copyright __MyCompanyName__ 2017. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
