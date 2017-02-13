59de2f6a603291512e950293e087b0e4c0799c68
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2017-02-13 23:30:31 +0000

    10.3 mac update
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/gap/trunk@3398 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/StepSync/FileObject.h
+++ system-apps/StepSync/FileObject.h
@@ -25,6 +25,9 @@
 
 #import <Foundation/Foundation.h>
 
+#if !defined (GNUSTEP) &&  (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4)
+typedef unsigned NSUInteger;
+#endif
 
 @interface FileObject : NSObject
 {
