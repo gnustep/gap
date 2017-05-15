9e8bd9e56018d1aff0aefd29e51fee6167a86c22
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2017-05-15 16:23:00 +0000

    Make containsString conditional, since it is present in GNUstep and since MacOS 10.10
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3443 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/Zipper/NSString+Custom.h
+++ system-apps/Zipper/NSString+Custom.h
@@ -1,8 +1,33 @@
+/*
+
+  NSString+Custom.h
+  Zipper
+
+  Copyright (C) 2012-2017 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/Foundation.h>
 
 @interface NSString (Convenience)
 
+#if !defined (GNUSTEP) &&  (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_9)
 - (BOOL)containsString:(NSString *)string;
+#endif
+
 - (BOOL)isEmpty;
 - (NSString *)stringByRemovingWhitespaceFromBeginning;
 
--- system-apps/Zipper/NSString+Custom.m
+++ system-apps/Zipper/NSString+Custom.m
@@ -1,12 +1,36 @@
+/*
+
+  NSString+Custom.m
+  Zipper
+
+  Copyright (C) 2012-2017 Free Software Foundation, Inc
+
+  Authors: Dirk Olmes <dirk@xanthippe.ping.de>
+           Riccardo Mottola <rm@gnu.org>
+
+  This application is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details
+
+ */
+
 #import <Foundation/Foundation.h>
 #import "NSString+Custom.h"
 
 @implementation NSString (Convenience)
 
+#if !defined (GNUSTEP) &&  (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_9)
 - (BOOL)containsString:(NSString *)string
 {
     return (([self rangeOfString:string]).length > 0);
 }
+#endif
 
 - (BOOL)isEmpty
 {
