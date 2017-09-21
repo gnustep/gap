b87742d633e3527f3328f513ea3789dc875586b7
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2017-09-21 21:25:19 +0000

    add file writer superclass
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3474 d25b4930-3292-e311-b074-0010c6de5da8

---
+++ libs/DataBasinKit/DBFileWriter.h
@@ -0,0 +1,49 @@
+/* -*- mode: objc -*-
+ Project: DataBasin
+ 
+ Copyright (C) 2009-2014 Free Software Foundation
+ 
+ Author: Riccardo Mottola
+ 
+ Created: 2017-09-20
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02111 USA.
+*/
+
+
+#import <Foundation/Foundation.h>
+
+@protocol DBLoggerProtocol;
+
+
+@interface DBFileWriter : NSObject
+{
+  id<DBLoggerProtocol> logger;
+  NSArray      *fieldNames;
+  NSArray      *fieldTypes;
+  NSFileHandle *file;
+}
+
+- (id)initWithHandle:(NSFileHandle *)fileHandle;
+- (void)setLogger:(id<DBLoggerProtocol>)l;
+- (void)setFieldNames: (id)obj andWriteThem: (BOOL)flag;
+- (void)writeStart;
+- (void)writeEnd;
+- (void)writeDataSet:(NSArray *)array;
+- (NSString *)formatOneLine:(id)data forHeader:(BOOL) headerFlag;
+
+
+@end
---
+++ libs/DataBasinKit/DBFileWriter.m
@@ -0,0 +1,54 @@
+/* -*- mode: objc -*-
+ Project: DataBasin
+ 
+ Copyright (C) 2009-2014 Free Software Foundation
+ 
+ Author: Riccardo Mottola
+ 
+ Created: 2017-09-20
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free
+ Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02111 USA.
+*/
+
+#import "DBFileWriter.h"
+
+
+@implementation DBFileWriter
+
+- (id)initWithHandle:(NSFileHandle *)fileHandle
+{
+  if ((self = [super init]))
+    {
+      file = fileHandle;
+      fieldNames = nil;
+    }
+  return self;
+}
+
+
+- (void)dealloc
+{
+  [fieldNames release];
+  [super dealloc];
+}
+
+
+- (void)setLogger:(id<DBLoggerProtocol>)l
+{
+  logger = l;
+}
+
+@end
