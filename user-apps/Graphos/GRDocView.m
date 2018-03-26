88bc22ed82328cc2c6e385994ce7461dc19c0708
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2018-03-26 13:05:16 +0000

    Update view after subdiving the path
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3620 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/Graphos/GRDocView.m
+++ user-apps/Graphos/GRDocView.m
@@ -1137,6 +1137,7 @@ float zFactors[ZOOM_FACTORS] = {0.25, 0.33, 0.5, 0.66, 0.75, 1, 1.25, 1.5, 2, 2.
             }
         }
     }
+  [self setNeedsDisplay:YES];
 }
 
 - (NSDictionary *)selectionProperties
