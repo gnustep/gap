2973a50966fd91121530a9836017e00f88a7d52e
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2020-01-15 00:55:35 +0000

    Update bilinear scaling from PRICE.
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@3773 d25b4930-3292-e311-b074-0010c6de5da8

--- user-apps/LaternaMagica/PRScale.m
+++ user-apps/LaternaMagica/PRScale.m
@@ -3,7 +3,7 @@
 //  PRICE
 //
 //  Created by Riccardo Mottola on Wed Jan 19 2005.
-//  Copyright (c) 2005-2017 Riccardo Mottola. All rights reserved.
+//  Copyright (c) 2005-2020 Riccardo Mottola. All rights reserved.
 //
 // This application is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -97,7 +97,7 @@
         for (y = 0; y < sizeY; y++)
             for (x = 0; x < sizeX; x++)
                 for (i = 0; i < srcSamplesPerPixel; i++)
-                    destData[destBytesPerRow * y + destBytesPerPixel * x + i] = srcData[srcBytesPerRow * (int)(y * yRatio)  + srcBytesPerPixel * (int)(x * xRatio) + i];
+                    destData[destBytesPerRow * y + destBytesPerPixel * x + i] = srcData[srcBytesPerRow * (int)floorf(y * yRatio)  + srcBytesPerPixel * (int)floorf(x * xRatio) + i];
       } 
     else if (method == BILINEAR)
       {
@@ -105,37 +105,57 @@
 	  w,h : original width and height
 	  v1, v2, v3, 4: four original corner values
 	  v' : new computed value
+
+          v3  v4
+          v1  v2      
           
 	  v' = v1(1-w)(1-h) + v2(w)(1-h) + v3(h)(1-w) + v3(w)(h)
 	*/
-        int v1, v2, v3, v4;
-        register int x0, y0;
         
         for (y = 0; y < sizeY; y++)
 	  for (x = 0; x < sizeX; x++)
 	    {
+              register NSInteger x0, y0;
 	      register float xDiff, yDiff;
 	      float xFloat, yFloat;
-              
+              NSInteger x1, y1;
+	      int xStep, yStep;
+
+	      // we use integer part of the ratio, so that we can set the next pixel to at least one apart
+	      xStep = floorf(xRatio);
+	      yStep = floorf(yRatio);
+	      if (xStep == 0) xStep = 1;
+	      if (yStep == 0) yStep = 1;
 	      xFloat = (float)x * xRatio;
 	      yFloat = (float)y * yRatio;
-	      x0 = (int)(xFloat);
-	      y0 = (int)(yFloat);
-	      xDiff = (xFloat - x0);
-	      yDiff = (yFloat - y0);
+	      x0 = (NSInteger)floorf(xFloat);
+	      y0 = (NSInteger)floorf(yFloat);
+              x1 = x0 + xStep;
+              y1 = y0 + yStep;
+
+	      // these are the weight w and h, normalized to the distance 1 : x1-x0
+	      xDiff = (xFloat - (float)x0)/(float)xStep;
+	      yDiff = (yFloat - (float)y0)/(float)yStep;
+
+              if (x1 >= origW )
+		{
+                  x1 = origW-1;
+		  xDiff = 0;
+		}
+              if (y1 >= origH )
+		{
+                  y1 = origH-1;
+		  yDiff = 0;
+		}
+              
 	      for (i = 0; i < srcSamplesPerPixel; i++)
 		{
-                  v1 = 0;
-                  v2 = 0;
-                  v3 = 0;
-                  v4 = 0;
+                  int v1, v2, v3, v4;
+
 		  v1 = srcData[srcBytesPerRow * y0 + srcBytesPerPixel * x0 + i];
-                  if (x < sizeX-1 )
-                    v2 = srcData[srcBytesPerRow * y0 + srcBytesPerPixel * (x0+1) + i];
-                  if (y < sizeY-1 )
-                    v3 = srcData[srcBytesPerRow * (y0+1) + srcBytesPerPixel * x0 + i];
-                  if ((x < sizeX-1) && (y < sizeY-1))
-                    v4 = srcData[srcBytesPerRow * (y0+1) + srcBytesPerPixel * (x0+1) + i];
+                  v2 = srcData[srcBytesPerRow * y0 + srcBytesPerPixel * x1 + i];
+                  v3 = srcData[srcBytesPerRow * y1 + srcBytesPerPixel * x0 + i];
+                  v4 = srcData[srcBytesPerRow * y1 + srcBytesPerPixel * x1 + i];
 
 		  destData[destBytesPerRow * y + destBytesPerPixel * x + i] = \
 		    (int)(v1*(1-xDiff)*(1-yDiff) + \
