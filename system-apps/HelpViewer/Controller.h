cfdccf562e946bca460c916d22e35739b9194ac0
rmottola <rmottola@d25b4930-3292-e311-b074-0010c6de5da8>
2026-02-26 19:55:34 +0000

    Transform into Document Based application
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@4147 d25b4930-3292-e311-b074-0010c6de5da8

--- system-apps/HelpViewer/Controller.h
+++ system-apps/HelpViewer/Controller.h
@@ -23,34 +23,14 @@
 #define __CONTROLLER_H__
 
 #import <AppKit/AppKit.h>
-#import "mainWindowController.h"
 #import "GNUstep.h"
 
 @interface Controller : NSObject
 {
-  id query;
-  id search;
-  id index;
-  id back;
-  id bookshelf;
-    
-  id textview;
-  id tocview;
-  id window;
-  
   id infoMenu;
   id helpMenu;
   id servicesMenu;
-  
-  MainWindowController* windowController;
 }
-- (void) openFile: (id) sender;
-- (void) search: (id) sender;
-- (void) index: (id) sender;
-- (void) back: (id) sender;
-- (void) bookshelf: (id) sender;
-- (void) print: (id) sender;
-- (void) initButtons;
 
 @end
 
--- system-apps/HelpViewer/Controller.m
+++ system-apps/HelpViewer/Controller.m
@@ -24,105 +24,25 @@
 
 @implementation Controller
 
-- (void) initButtons 
-{
-  [search setTitle: _(@"Search")];
-  [search setFont: [NSFont systemFontOfSize: 0]];
-  [search setImagePosition: NSImageAbove];
-  [search setImage: [NSImage imageNamed: @"Search.tiff"]];
 
-  [index setTitle: _(@"Index")];
-  [index setFont: [NSFont systemFontOfSize: 0]];
-  [index setImagePosition: NSImageAbove];
-  [index setImage: [NSImage imageNamed: @"Index.tiff"]];
-
-  [back setTitle: _(@"Back")];
-  [back setFont: [NSFont systemFontOfSize: 0]];
-  [back setImagePosition: NSImageAbove];
-  [back setImage: [NSImage imageNamed: @"Back.tiff"]];
-
-  [bookshelf setTitle: _(@"Bookshelf")];
-  [bookshelf setFont: [NSFont systemFontOfSize: 0]];
-  [bookshelf setImagePosition: NSImageAbove];
-  [bookshelf setImage: [NSImage imageNamed: @"Bookshelf.tiff"]];
-}
 
 - (void) awakeFromNib
 {
-  windowController = [[MainWindowController alloc] initWithTextView: textview
-						     andBrowserView: tocview];
-   
+/*
   [infoMenu setAction: @selector (orderFrontStandardInfoPanel:)];
   [helpMenu setAction: @selector (orderFrontHelpPanel:)];
-
-  [self initButtons];
-
-  [window setTitle: @"HelpViewer"];
-  [windowController setWindow: window];
-
-  [NSApp setDelegate: self];
+ */
 }
 
-- (void) applicationDidFinishLaunching: (NSNotification *) not
+- (void) applicationDidFinishLaunching: (NSNotification *)aNot
 {
-  NSArray *args = [[NSProcessInfo processInfo] arguments];
 
-  if ([args count] > 1)
-    {
-      if ([[NSFileManager defaultManager] fileExistsAtPath: [args objectAtIndex: 1]])
-	{
-	  [windowController loadFile: [args objectAtIndex: 1]];
-	}
-    }
-}
-
-- (BOOL)application:(NSApplication *)theApplication openFile:(NSString *)filename {
-  return [windowController loadFile: filename];
 }
 
 - (void) dealloc 
 {
-  RELEASE (windowController);
   [super dealloc];
 }
 
-- (void) openFile: (id) sender
-{
-  NSInteger ret;
-  NSOpenPanel* panel;
-
-  panel = [NSOpenPanel openPanel];
-  [panel setAllowsMultipleSelection: NO];
-  ret = [panel runModalForTypes: [NSArray arrayWithObject: @"help"]];
-  if (ret == NSOKButton)
-    {
-      [windowController loadFile: [[panel filenames] objectAtIndex: 0]];
-    }
-}
-
-- (void) search: (id) sender
-{
-    NSLog (@"Search ...");
-}
-
-- (void) index: (id) sender 
-{
-    NSLog (@"Index ...");
-}
-
-- (void) back: (id) sender
-{
-    NSLog (@"Back ...");
-}
-
-- (void) bookshelf: (id) sender
-{
-    NSLog (@"Bookshelf ...");
-}
-
-- (void) print: (id) sender
-{
-  [windowController print: sender];
-}
 
 @end
