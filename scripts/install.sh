622ed5699d3c0613ada7d17571683f41ed3387c1
gcasa <gcasa@d25b4930-3292-e311-b074-0010c6de5da8>
2026-09-05 05:00:16 +0000

    Get FlexiSheet working on GNUstep
    git-svn-id: svn://svn.savannah.nongnu.org/gap/trunk@4279 d25b4930-3292-e311-b074-0010c6de5da8

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-. /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
-make GNUSTEP_INSTALLATION_DOMAIN=LOCAL install
+set -e
 
-exit 0
+cd "$(dirname "$0")/.."
+
+. /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
+
+(cd libs/netclasses && ./configure)
+
+INSTALL_DOMAIN="${GNUSTEP_INSTALLATION_DOMAIN:-LOCAL}"
+
+if [ "$INSTALL_DOMAIN" != "USER" ] && [ "$(id -u)" -ne 0 ]; then
+  echo "Installing to the $INSTALL_DOMAIN GNUstep domain requires elevated permissions." >&2
+  echo "Run this script with sudo, or set GNUSTEP_INSTALLATION_DOMAIN=USER." >&2
+  exit 1
+fi
+
+make GNUSTEP_INSTALLATION_DOMAIN="$INSTALL_DOMAIN" install
